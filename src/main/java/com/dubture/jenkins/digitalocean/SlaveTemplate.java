jenkinsci/digitalocean-plugin

commit c9fd09e53fd351eb5102f81b8628e7aa1d83801a

integrate with cloud-stats (#31)
the cloud-stats plugin is fairly handy to track cloud availability. for it
to work we'll need to pass a ProvisioningActivity id from the cloud all
the way through to the Computer. Slave and Computer become TrackedItems.

src/main/java/com/dubture/jenkins/digitalocean/SlaveTemplate.java
@@ -3,6 +3,7 @@
  *
  * Copyright (c) 2014 robert.gruendler@dubture.com
  *               2016 Maxim Biro <nurupo.contributions@gmail.com>
+ *               2017 Harald Sitter <sitter@kde.org>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -55,6 +56,7 @@ import hudson.slaves.NodeProperty;
 import hudson.util.FormValidation;
 import hudson.util.ListBoxModel;
 import jenkins.model.Jenkins;
+import org.jenkinsci.plugins.cloudstats.ProvisioningActivity;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.QueryParameter;
 
@@ -198,7 +200,13 @@ public class SlaveTemplate implements Describable<SlaveTemplate> {
         return count >= instanceCap;
     }
 
-    public Slave provision(String dropletName, String cloudName, String authToken, String privateKey, Integer sshKeyId, List<Droplet> droplets)
+    public Slave provision(ProvisioningActivity.Id provisioningId,
+                           String dropletName,
+                           String cloudName,
+                           String authToken,
+                           String privateKey,
+                           Integer sshKeyId,
+                           List<Droplet> droplets)
             throws IOException, RequestUnsuccessfulException, Descriptor.FormException {
 
         LOGGER.log(Level.INFO, "Provisioning slave...");
@@ -227,7 +235,7 @@ public class SlaveTemplate implements Describable<SlaveTemplate> {
             DigitalOceanClient apiClient = new DigitalOceanClient(authToken);
             Droplet createdDroplet = apiClient.createDroplet(droplet);
 
-            return newSlave(cloudName, createdDroplet, privateKey);
+            return newSlave(provisioningId, cloudName, createdDroplet, privateKey);
         } catch (Exception e) {
             LOGGER.log(Level.WARNING, e.getMessage(), e);
             throw new AssertionError();
@@ -242,9 +250,10 @@ public class SlaveTemplate implements Describable<SlaveTemplate> {
      * @throws IOException
      * @throws Descriptor.FormException
      */
-    private Slave newSlave(String cloudName, Droplet droplet, String privateKey) throws IOException, Descriptor.FormException {
+    private Slave newSlave(ProvisioningActivity.Id provisioningId, String cloudName, Droplet droplet, String privateKey) throws IOException, Descriptor.FormException {
         LOGGER.log(Level.INFO, "Creating new slave...");
         return new Slave(
+                provisioningId,
                 cloudName,
                 droplet.getName(),
                 "Computer running on DigitalOcean with name: " + droplet.getName(),

pom.xml
@@ -76,6 +76,11 @@
             <artifactId>guava</artifactId>
             <version>15.0</version>
         </dependency>
+        <dependency>
+            <groupId>org.jenkins-ci.plugins</groupId>
+            <artifactId>cloud-stats</artifactId>
+            <version>0.9</version>
+        </dependency>
     </dependencies>
 
     <build>

src/main/java/com/dubture/jenkins/digitalocean/Cloud.java
@@ -3,6 +3,7 @@
  *
  * Copyright (c) 2014 robert.gruendler@dubture.com
  *               2016 Maxim Biro <nurupo.contributions@gmail.com>
+ *               2017 Harald Sitter <sitter@kde.org>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -40,6 +41,8 @@ import hudson.slaves.NodeProvisioner;
 import hudson.util.FormValidation;
 import hudson.util.ListBoxModel;
 import jenkins.model.Jenkins;
+import org.jenkinsci.plugins.cloudstats.ProvisioningActivity;
+import org.jenkinsci.plugins.cloudstats.TrackedPlannedNode;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.QueryParameter;
 
@@ -221,7 +224,8 @@ public class Cloud extends hudson.slaves.Cloud {
 
                     final String dropletName = DropletName.generateDropletName(name, template.getName());
 
-                    provisioningNodes.add(new NodeProvisioner.PlannedNode(dropletName, Computer.threadPoolForRemoting.submit(new Callable<Node>() {
+                    final ProvisioningActivity.Id provisioningId = new ProvisioningActivity.Id(this.name, template.getName(), dropletName);
+                    provisioningNodes.add(new TrackedPlannedNode(provisioningId, template.getNumExecutors(), Computer.threadPoolForRemoting.submit(new Callable<Node>() {
                         public Node call() throws Exception {
                             Slave slave;
                             synchronized (provisionSynchronizor) {
@@ -231,13 +235,14 @@ public class Cloud extends hudson.slaves.Cloud {
                                     LOGGER.log(Level.INFO, "Instance cap reached, not provisioning.");
                                     return null;
                                 }
-                                slave = template.provision(dropletName, name, authToken, privateKey, sshKeyId, droplets);
+                                slave = template.provision(provisioningId, dropletName, name, authToken, privateKey,
+                                                           sshKeyId, droplets);
                             }
                             Jenkins.getInstance().addNode(slave);
                             slave.toComputer().connect(false).get();
                             return slave;
                         }
-                    }), template.getNumExecutors()));
+                    })));
 
                     excessWorkload -= template.getNumExecutors();
                 }

src/main/java/com/dubture/jenkins/digitalocean/Computer.java
@@ -3,6 +3,7 @@
  *
  * Copyright (c) 2014 robert.gruendler@dubture.com
  *               2016 Maxim Biro <nurupo.contributions@gmail.com>
+ *               2017 Harald Sitter <sitter@kde.org>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -30,6 +31,8 @@ import com.myjeeva.digitalocean.exception.RequestUnsuccessfulException;
 import com.myjeeva.digitalocean.impl.DigitalOceanClient;
 import com.myjeeva.digitalocean.pojo.Droplet;
 import hudson.slaves.AbstractCloudComputer;
+import org.jenkinsci.plugins.cloudstats.ProvisioningActivity;
+import org.jenkinsci.plugins.cloudstats.TrackedItem;
 
 import java.util.logging.Logger;
 
@@ -41,9 +44,11 @@ import java.util.logging.Logger;
  *
  * @author robert.gruendler@dubture.com
  */
-public class Computer extends AbstractCloudComputer<Slave> {
+public class Computer extends AbstractCloudComputer<Slave> implements TrackedItem {
 
     private static final Logger LOGGER = Logger.getLogger(Computer.class.getName());
+
+    private final ProvisioningActivity.Id provisioningId;
 
     private final String authToken;
 
@@ -51,6 +56,7 @@ public class Computer extends AbstractCloudComputer<Slave> {
 
     public Computer(Slave slave) {
         super(slave);
+        provisioningId = slave.getId();
         dropletId = slave.getDropletId();
         authToken = slave.getCloud().getAuthToken();
     }
@@ -66,6 +72,11 @@ public class Computer extends AbstractCloudComputer<Slave> {
 
         LOGGER.info("Slave removed, deleting droplet " + dropletId);
         DigitalOcean.tryDestroyDropletAsync(authToken, dropletId);
+    }
+
+    @Override
+    public ProvisioningActivity.Id getId() {
+        return provisioningId;
     }
 
     public Cloud getCloud() {

src/main/java/com/dubture/jenkins/digitalocean/Slave.java
@@ -3,6 +3,7 @@
  *
  * Copyright (c) 2014 robert.gruendler@dubture.com
  *               2016 Maxim Biro <nurupo.contributions@gmail.com>
+ *               2017 Harald Sitter <sitter@kde.org>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -33,6 +34,8 @@ import hudson.slaves.ComputerLauncher;
 import hudson.slaves.NodeProperty;
 import hudson.slaves.RetentionStrategy;
 import jenkins.model.Jenkins;
+import org.jenkinsci.plugins.cloudstats.ProvisioningActivity;
+import org.jenkinsci.plugins.cloudstats.TrackedItem;
 
 import java.io.IOException;
 import java.util.List;
@@ -49,9 +52,11 @@ import java.util.logging.Logger;
  *
  * @author robert.gruendler@dubture.com
  */
-public class Slave extends AbstractCloudSlave {
+public class Slave extends AbstractCloudSlave implements TrackedItem {
 
     private static final Logger LOG = Logger.getLogger(Slave.class.getName());
+
+    private final ProvisioningActivity.Id provisioningId;
 
     private final String cloudName;
 
@@ -74,7 +79,7 @@ public class Slave extends AbstractCloudSlave {
     /**
      * {@link Slave}s are created by {@link SlaveTemplate}s
      */
-    public Slave(String cloudName, String name, String nodeDescription, Integer dropletId, String privateKey,
+    public Slave(ProvisioningActivity.Id provisioningId, String cloudName, String name, String nodeDescription, Integer dropletId, String privateKey,
                  String remoteAdmin, String remoteFS, int sshPort, int numExecutors, int idleTerminationTime,
                  Mode mode, String labelString, ComputerLauncher launcher, RetentionStrategy retentionStrategy,
                  List<? extends NodeProperty<?>> nodeProperties, String initScript, String jvmOpts)
@@ -82,6 +87,7 @@ public class Slave extends AbstractCloudSlave {
 
         super(name, nodeDescription, remoteFS, numExecutors, mode, labelString, launcher, retentionStrategy, nodeProperties);
 
+        this.provisioningId = provisioningId;
         this.cloudName = cloudName;
         this.dropletId = dropletId;
         this.privateKey = privateKey;
@@ -144,6 +150,11 @@ public class Slave extends AbstractCloudSlave {
     @Override
     protected void _terminate(TaskListener listener) throws IOException, InterruptedException {
         DigitalOcean.tryDestroyDropletAsync(getCloud().getAuthToken(), dropletId);
+    }
+
+    @Override
+    public ProvisioningActivity.Id getId() {
+        return provisioningId;
     }
 
     public long getStartTimeMillis() {

commit cec2afce7f0d52361577f759c2e6d38cf626db35

give errors thrown during provision useful context
cloud-stats introduced in c9fd09e53fd351eb5102f81b8628e7aa1d83801a track
the errors which caused a failed provisioning attempt allowing to easily
identify what the problem was. but to do that one needs a useful context
message.

so, instead of only logging the error causes log them AND then use them
also as message for the thrown error.

src/main/java/com/dubture/jenkins/digitalocean/SlaveTemplate.java
@@ -61,6 +61,7 @@ import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.QueryParameter;
 
 import static com.google.common.collect.Lists.newArrayList;
+import static java.lang.String.format;
 
 /**
  * A {@link SlaveTemplate} represents the configuration values for creating a new slave via a DigitalOcean droplet.
@@ -216,7 +217,9 @@ public class SlaveTemplate implements Describable<SlaveTemplate> {
             LOGGER.log(Level.INFO, "Starting to provision digital ocean droplet using image: " + imageId + " region: " + regionId + ", sizeId: " + sizeId);
 
             if (isInstanceCapReachedLocal(cloudName) || isInstanceCapReachedRemote(droplets, cloudName)) {
-                throw new AssertionError();
+                String msg = format("instance cap reached for %s in %s", dropletName, cloudName);
+                LOGGER.log(Level.INFO, msg);
+                throw new AssertionError(msg);
             }
 
             // create a new droplet
@@ -239,8 +242,9 @@ public class SlaveTemplate implements Describable<SlaveTemplate> {
 
             return newSlave(provisioningId, cloudName, createdDroplet, privateKey);
         } catch (Exception e) {
-            LOGGER.log(Level.WARNING, e.getMessage(), e);
-            throw new AssertionError();
+            String msg = format("Unexpected error raised during provisioning of %s:\n%s", dropletName, e.getMessage());
+            LOGGER.log(Level.WARNING,  msg, e);
+            throw new AssertionError(msg);
         }
     }
 